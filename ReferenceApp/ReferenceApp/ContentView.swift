fdd89f4eaf9ffe934abc9e1429f6f566ac096d4b
Gio Lodi <giovanni.lodi42@gmail.com>
2020-09-16 20:11:51 +1000

    Track a template iOS app for reference

---
+++ ReferenceApp/ReferenceApp/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  ReferenceApp
+//
+//  Created by Gio on 16/9/20.
+//  Copyright © 2020 Gio. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
